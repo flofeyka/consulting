c8621b7043c3971e341199a79ed4dd04888aea58
flofeyka <danil.bashirov.08@gmail.com>
2024-07-15 10:47:56 +0300

    Team photos reposition. Two team members added.

--- src/Pages/MainPage/Sections/Practices/const.ts
+++ src/Pages/MainPage/Sections/Practices/const.ts
@@ -1,10 +1,8 @@
 import { SectionType } from '@/types';
 
 export const PRACTICES_DROPDOWN_SECTIONS = [
-  SectionType.Banking,
   SectionType.Efficiency,
   SectionType.Government,
-  SectionType.InnovationProjects,
   SectionType.Investing,
   SectionType.Marketing,
   SectionType.Strategy,
